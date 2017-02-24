c66356d596564cf216bf19d446f9212c9335e9e3
King Zhang <gnilihzeux@sina.cn>
2017-02-24 21:09:18 +0800

    Update barplot.R

--- barplot.R
+++ barplot.R
@@ -3,7 +3,7 @@
 ###############################
 # 参考 Quick-R: http://www.statmethods.net/graphs/bar.html
 
-###--- 简单情况 ----------------
+###--- 简单情况 --------------------------------------------------
 # 基本
 counts <- table(mtcars$gear)
 barplot(counts, main="Car Distribution", xlab="Number of Gears")
@@ -24,21 +24,21 @@ par(mar=c(5,8,4,2)) # increase y-axis margin.
 counts <- table(mtcars$gear)
 barplot(counts, main="Car Distribution", horiz=TRUE, names.arg=c("3 Gears", "4 Gears", "5   Gears"), cex.names=0.8)
 
-###--- 重叠（stacked）----------
+###--- 重叠（stacked）-------------------------------------------
 # 并附上legend；其实就是数据变换了格式
 counts <- table(mtcars$vs, mtcars$gear)
 barplot(counts, main="Car Distribution by Gears and VS",
   xlab="Number of Gears", col=c("darkblue","red"),
  	legend = rownames(counts))
   
-###--- 分组 -------------------
+###--- 分组 -----------------------------------------------------
 # 设置了参数beside
 counts <- table(mtcars$vs, mtcars$gear)
 barplot(counts, main="Car Distribution by Gears and VS",
   xlab="Number of Gears", col=c("darkblue","red"),
  	legend = rownames(counts), beside=TRUE)
 
-###--- 加标准误 ----------------
+###--- 加标准误 --------------------------------------------------
 # 实际上通过arrow函数拼接上去的
 # 数据构造
 tbl <- data.frame(groups= sample(rep(1:3, 100), 30),
@@ -57,4 +57,18 @@ x_loc <- 0.5*(1 + seq(from= 0, by= 2, length.out= grp))  + space*seq_len(grp)
 arrows(x0=x_loc, x1= x_loc, y0= mean_tbl[, 2], y1= mean_tbl[, 2] + sd_tbl[, 2], angle= 90, length= .05)
 arrows(x0=x_loc, x1= x_loc, y0= mean_tbl[, 2], y1= mean_tbl[, 2] - sd_tbl[, 2], angle= 90, length= .05)
 
-###--- 不同组别坐标轴对称分布 ---
+###--- 不同组别坐标轴对称分布 ---------------------------------------
+##############
+#### 讨论   ###
+##############
+# 1.非多组(beside= F), 如<加标准误>模块，关于barplot(horiz= FALSE)非多组的x轴的位置，第一个bar的左边缘的x值为参数space的距离，
+#   然后每一个bar的宽度刚好为1(其中width= 1控制），所以可以如<加标准误>的例子中定位每个bar的中心；
+counts <- table(mtcars$gear)
+points(c(0.7, 1.9), c(2, 2))
+# 该例子是默认参数，我们可以看出默认space= 0.2, 那么第一个bar的x坐标为0.2，原点分别标示了第一以及第二个bar的x位置
+
+# 2.多组(beside= T)的情况，即<分组>模块，第一个bar的x轴坐标的距离刚好是1，且不同组之间的间隔也为1(其中参数width= 1)
+counts <- table(mtcars$vs, mtcars$gear)
+barplot(counts, beside= T, col= "grey")
+points(c(1.5, 2.5, 3.5, 4.5), rep(2, 4))
+# 可以从该例子中看出，默认space=1，所以第一个bar的x坐标也为1
