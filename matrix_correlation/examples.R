22fd37f2bb470784dd867f43fc67ec19a0881299
King Zhang <gnilihzeux@sina.cn>
2017-04-08 22:11:12 +0800

    Create examples.R

---
+++ matrix_correlation/examples.R
@@ -0,0 +1,40 @@
+########################################################
+###　Examples to display how to use  matrixCorTest.R ###
+########################################################
+###--- header ------------------------------------------
+setwd("E:/King组内工作/好的程序/cor.test")
+source("cor.test.pearson.R")
+source("matrixCorTest.R")
+options(stringsAsFactors= F)
+
+###--- input -------------------------------------------
+gn <- read.table("protein_expression_profile.txt", header= T, sep= "\t")
+lnc <- read.table("lncRNA_expression_profile.txt", header= T, sep= "\t")
+
+###--- calculate correlations between lncRNAs and genes -
+# calculate cor, p and fdr
+# the bigger matrix as 'x'
+mat <- pearsonTestMatrix(t(gn), t(lnc))
+# display
+class(mat)
+names(mat)
+
+# convert result into data.frame
+# set 'symmetric', here is not.
+rslt <- outputDF(mat, symmetric= F)
+
+# estimate with 
+checkPoint(gn, lnc, rslt)
+
+###--- calculate correlation between genes --------------
+mat <- pearsonTestMatrix(t(gn), size= 1000)
+# display
+class(mat)
+names(mat)
+
+# convert result into data.frame
+# set 'symmetric', here is not.
+rslt <- outputDF(mat, symmetric= F)
+
+# estimate with 
+checkPoint(gn, lnc, rslt)
